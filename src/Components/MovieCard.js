dd5c1a1b899cabedaf476983fd61dbe5e6f776f9
Amany Siraj Al-Din <amanysirajaldin@email.com>
2021-10-29 20:56:18 +0200

    Update Movie Card Style

--- src/Components/MovieCard.js
+++ src/Components/MovieCard.js
@@ -1,18 +1,23 @@
 import { Card, Button } from "react-bootstrap";
 
+import "./MovieCard.css"
+
 export default function MovieItem({ title, poster, release_date }) {
     //const { title, poster } = props;
+    const toggelOverView = () => {
+
+    }
     return (
         <div className="col-lg-3 col-md-4 col-6 d-flex my-3">
             <Card className="shadow flex-fill">
                 <Card.Img variant="top" src={poster} />
-                <Card.Body>
+                <Card.Body onMouseOver={toggelOverView} onMouseOut={toggelOverView}>
                     <Card.Title>{title}</Card.Title>
                     <Card.Text>
                         {release_date}
                     </Card.Text>
-                    <Button variant="primary">Go somewhere</Button>
                 </Card.Body>
+                <Button variant="primary">Go somewhere</Button>
             </Card>
         </div>
     );
