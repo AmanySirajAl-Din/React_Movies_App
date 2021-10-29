29e1649eb3dc95e0dc0000a71a3523aea02fd94d
Amany Siraj Al-Din <amanysirajaldin@email.com>
2021-10-29 15:13:46 +0200

    Add release_date to movie card

--- src/Components/MovieCard.js
+++ src/Components/MovieCard.js
@@ -2,7 +2,7 @@ import { Card, Button } from "react-bootstrap";
 
 import "./MovieCard.css"
 
-export default function MovieItem({ title, poster, overview }) {
+export default function MovieItem({ title, poster, release_date }) {
     //const { title, poster } = props;
     return (
         <Card className="col-lg-3 col-md-4 col-5 shadow m-3">
@@ -10,7 +10,7 @@ export default function MovieItem({ title, poster, overview }) {
             <Card.Body>
                 <Card.Title>{title}</Card.Title>
                 <Card.Text>
-                    {overview}
+                    {release_date}
                 </Card.Text>
                 <Button variant="primary">Go somewhere</Button>
             </Card.Body>
