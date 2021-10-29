c8469d682011dad8a322a4e077f3640e6d7587ea
Amany Siraj Al-Din <amanysirajaldin@email.com>
2021-10-29 22:02:57 +0200

    Fix movie in props error

--- src/Components/MovieCard.js
+++ src/Components/MovieCard.js
@@ -2,21 +2,22 @@ import { Card, Button } from "react-bootstrap";
 
 import "./MovieCard.css"
 
-export default function MovieItem(movie) {
+export default function MovieItem({ movie }) {
     //const { title, poster } = props;
+
     const toggelOverView = () => {
 
     }
     return (
         <div className="col-lg-3 col-md-4 col-6 d-flex my-3">
             <Card className="shadow flex-fill">
-                <Card.Img variant="top" src={movie.poster} />
+                <Card.Img variant="top" src={movie.poster_path} />
                 <Card.Body onMouseOver={toggelOverView} onMouseOut={toggelOverView}>
                     <Card.Title>{movie.title}</Card.Title>
                     <Card.Text>
                         {movie.release_date}
-                        
                     </Card.Text>
+
                 </Card.Body>
                 <Button variant="primary">Go somewhere</Button>
             </Card>
