f6bdc82baff0905d5116289778fee7724fac4872
Amany Siraj Al-Din <amanysirajaldin@email.com>
2021-10-29 23:00:58 +0200

    Remove overview

--- src/Components/MovieCard.js
+++ src/Components/MovieCard.js
@@ -1,25 +1,16 @@
 import { Card, Button } from "react-bootstrap";
 
-import "./MovieCard.css"
-
 export default function MovieItem({ movie }) {
     //const { title, poster } = props;
-
-    const toggelOverView = () => {
-
-    }
     return (
         <div className="col-lg-3 col-md-4 col-6 d-flex my-3">
-            <Card className="shadow flex-fill">
+            <Card id={movie.id} className="shadow flex-fill">
                 <Card.Img variant="top" src={movie.poster_path} />
-                <Card.Body onMouseOver={toggelOverView} onMouseOut={toggelOverView}>
+                <Card.Body>
                     <Card.Title>{movie.title}</Card.Title>
                     <Card.Text>
                         {movie.release_date}
                     </Card.Text>
-                    <div className="Drawer" hidden>
-                        {movie.overview}
-                    </div>
                 </Card.Body>
                 <Button variant="primary">Go somewhere</Button>
             </Card>
