2d16f02f3485dcbe32ce2433b7525e1d59f3d390
Amany Siraj Al-Din <amanysirajaldin@email.com>
2021-10-29 05:29:15 +0200

    Update Navbar Structure

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,18 +1,24 @@
 import { Navbar, Container, Nav, NavDropdown } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
+import logo from "../assets/images/logo.png";
+
+import "./Navbar.css";
+
 export default function NavbarComponent() {
     return (
-        <Navbar bg="dark" expand="lg">
+        <Navbar bg="dark" variant="dark" expand="lg">
             <Container>
-                <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                <Navbar.Brand href="#home" className="navbar-brand">
+                    <img src={logo} className="w-100" alt="logo" />
+                </Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
-                        <Link to="/">Movies</Link>
-                        <Link to="/addMovies">Add Movies</Link>
-                        <Link to="/hooks">Hooks</Link>
-                        <Link to="/users">Users</Link>
+                        <Link className="mx-2" to="/">Movies</Link>
+                        <Link className="mx-2" to="/addMovies">Add Movies</Link>
+                        <Link className="mx-2" to="/hooks">Hooks</Link>
+                        <Link className="mx-2" to="/users">Users</Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
