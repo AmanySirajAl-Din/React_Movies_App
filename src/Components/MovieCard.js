0f3c5cc83482a19c952793420784fc62972235a8
Amany Siraj Al-Din <amanysirajaldin@email.com>
2021-10-29 15:20:40 +0200

    Update movie card style

--- src/Components/MovieCard.js
+++ src/Components/MovieCard.js
@@ -5,15 +5,17 @@ import "./MovieCard.css"
 export default function MovieItem({ title, poster, release_date }) {
     //const { title, poster } = props;
     return (
-        <Card className="col-lg-3 col-md-4 col-5 shadow m-3">
-            <Card.Img variant="top" src={poster} />
-            <Card.Body>
-                <Card.Title>{title}</Card.Title>
-                <Card.Text>
-                    {release_date}
-                </Card.Text>
-                <Button variant="primary">Go somewhere</Button>
-            </Card.Body>
-        </Card>
+        <div className="col-lg-3 col-md-4 col-6 d-flex my-3">
+            <Card className="shadow flex-fill">
+                <Card.Img variant="top" src={poster} />
+                <Card.Body>
+                    <Card.Title>{title}</Card.Title>
+                    <Card.Text>
+                        {release_date}
+                    </Card.Text>
+                    <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+            </Card>
+        </div>
     );
 };
